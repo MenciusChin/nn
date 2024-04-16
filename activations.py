b3c536841f127bc2c6ef641880b59f531b4af620
MenciusChin <menciuschin@gmail.com>
2024-04-16 15:02:27 -0400

    add activations

---
+++ activations.py
@@ -0,0 +1,13 @@
+"""
+Activation functions and its derivatives
+"""
+
+import numpy as np
+
+
+# activation function and its derivative
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_prime(x):
+    return 1-np.tanh(x)**2
