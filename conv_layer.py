d50218d9f8eedf0d448ffc0f980727aac323c56f
MenciusChin <menciuschin@gmail.com>
2024-04-16 23:56:25 -0400

    stuck at conv forward

--- conv_layer.py
+++ conv_layer.py
@@ -1,3 +1,31 @@
 """
 Convolution layers
 """
+
+import numpy as np
+from layer import Layer
+
+
+class Conv(Layer):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size=3,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=True
+    ) -> None:
+        
+        if isinstance(kernel_size, int):
+            # case when kernel size is int for square kernel
+            self.filters = np.random.randn(in_channels, out_channels, kernel_size, kernel_size)
+        else:
+            # case when kernel size is tuple
+            self.filters = np.random.randn(in_channels, out_channels, kernel_size[0], kernel_size[1]) 
+
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        
