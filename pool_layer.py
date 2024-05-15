af06ff928ce1b764348ddf5db9063632877fc021
MenciusChin <menciuschin@gmail.com>
2024-05-15 13:44:18 -0400

    add pool forward and tests

--- pool_layer.py
+++ pool_layer.py
@@ -6,17 +6,20 @@ import numpy as np
 import functional as F
 from layer import Layer
 import multiprocessing as mp
-# This is for windows
 from multiprocessing.pool import ThreadPool as Pool
 
+### Tasks ###
+# 1. write tests
 
-class MaxPool(Layer):
+
+class Pool(Layer):
     def __init__(
             self,
             kernel_size,
             stride=None,
             padding=0,
             dilation=1,
+            method="max",
             return_indices=False,
             ceil_mode=False
     ):
@@ -25,9 +28,11 @@ class MaxPool(Layer):
         stride = kernel_size if stride is None else stride
 
         # tuple all paramters
-        self.kernel_size, self.stride, self.padding, self.dilation = F.paramstuple(
+        self.kernel_size, self.stride, self.padding, self.dilation = F.paramstuple((
             kernel_size, stride, padding, dilation
-        )
+        ))
+
+        self.method = method
 
     def forward(self, input):
         """
@@ -37,9 +42,10 @@ class MaxPool(Layer):
         (1, channel, out_H, out_W)
         """
 
-        
+        self.input = input
 
-        return
+        return F.poolnto1(input, self.kernel_size, self.stride, self.padding,
+                          self.dilation, self.method)
 
     def backward(self, output_error, learning_rate):
         return
@@ -54,4 +60,11 @@ class MaxPool(Layer):
 
 if __name__ == "__main__":
     # testing code
-    print("hi")
+    data = np.random.randn(1, 3, 4, 4)
+    print("Original data")
+    print(data)
+    maxpool = Pool(kernel_size=(2, 2)) 
+
+    pool_result = maxpool.forward(data)
+    print("\nPool result")
+    print(pool_result)
