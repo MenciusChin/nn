c085119d8c4fa87f8683fab303f735e96443732a
MenciusChin <menciuschin@gmail.com>
2024-05-18 13:47:30 -0400

    add softmax

--- activations.py
+++ activations.py
@@ -2,10 +2,32 @@
 Activation functions and its derivatives.
 """
 
+### Tasks ###
+# 1. Add softmax division by dimension
+
+
 import numpy as np
+
 
 def tanh(x):
     return np.tanh(x)
 
+
 def tanh_prime(x):
     return 1-np.tanh(x)**2
+
+
+def softmax(x, dim=None):
+    e_x = np.exp(x - np.max(x))
+
+    # Implement softmax division by dimension/axis
+    return e_x / e_x.sum() if dim is None else e_x / e_x.sum(axis=dim)
+
+
+
+if __name__ == "__main__":
+    # test
+    scores = np.array([[1, 2, 3, 6],
+                       [2, 4, 5, 6],
+                       [3, 8, 7, 6]])
+    print(softmax(scores, dim=0))
