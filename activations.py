f1e06aca077ab74e83ca758a7e6852becc9d0f95
MenciusChin <menciuschin@gmail.com>
2024-05-18 20:35:21 -0400

    change in structure, now activations have their own layer strutures

--- activations.py
+++ activations.py
@@ -3,7 +3,7 @@ Activation functions and its derivatives.
 """
 
 ### Tasks ###
-# 1. Add softmax division by dimension
+# 1. Add softmax division by dimension (2+)
 
 
 import numpy as np
@@ -18,16 +18,19 @@ def tanh_prime(x):
 
 
 def softmax(x, dim=None):
+    # Now only implemented the 1D array calculation
     e_x = np.exp(x - np.max(x))
 
     # Implement softmax division by dimension/axis
     return e_x / e_x.sum() if dim is None else e_x / e_x.sum(axis=dim)
 
 
+#
+
+
+
 
 if __name__ == "__main__":
     # test
-    scores = np.array([[1, 2, 3, 6],
-                       [2, 4, 5, 6],
-                       [3, 8, 7, 6]])
+    scores = np.array([1, 2, 3, 6])
     print(softmax(scores, dim=0))
