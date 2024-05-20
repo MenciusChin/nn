afa9bcd4ce8691f6fb59f72b96b4e6e925a706e3
MenciusChin <menciuschin@gmail.com>
2024-05-19 21:53:54 -0400

    finish torch testing for fclayer except backwards

--- fc_layer.py
+++ fc_layer.py
@@ -18,8 +18,8 @@ class FCLayer(Layer):
         # initialize flatten to be false unless deteced
         self.flatten = None
 
-        self.weights = np.random.randn(input_size, output_size) * math.sqrt(2 / input_size)
-        self.bias = np.random.rand(1, output_size) - 0.5
+        self.weights = np.random.randn(input_size, output_size) * np.sqrt(2 / input_size)
+        self.bias = np.random.rand(1, output_size) - 0.5 if bias else None
     
     # returns output for a given input
     def forward(self, input_data):
@@ -27,11 +27,10 @@ class FCLayer(Layer):
             # if input shape have more than 1 dim
             self.flatten = input_data
             self.input = F.flatten(input_data)
-            self.output = self.input @ self.weights + self.bias
         else:
             # output = input @ weights + bias
             self.input = input_data
-            self.output = self.input @ self.weights + self.bias
+        self.output = self.input @ self.weights + self.bias if self.bias else self.input @ self.weights
         return self.output
 
     # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
@@ -48,17 +47,53 @@ class FCLayer(Layer):
 
         # update parameters
         self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
+        if self.bias:
+            self.bias -= learning_rate * output_error
 
         return input_error if self.flatten is False else np.reshape(input_error, self.flatten.shape)
+    
+
+    def set_weights(self, weights):
+        """
+        Set weights (mostly pretrained) for current layer
+        """
+        self.weights = weights
 
 
 if __name__ == "__main__":
-    # test
-    data = np.random.randn(1, 3, 3, 3)
-    fctest = FCLayer(27, 27)
+    # test imports
+    # utilize torch to test calculation
+    import torch
+    import torch.nn as nn
 
-    flatten_pass = fctest.forward(data)
+    # initialize input data
+    # shape (1, 3, 3, 3)
+    init_np = np.random.randn(1, 3, 3, 3)
+    # note the dtype here needs modification
+    init_tensor = torch.tensor(init_np, dtype=torch.float32)
 
-    reshaped_gradient = fctest.backward(np.random.randn(1, 27), learning_rate=.001)
-    print(reshaped_gradient)
+    fc = FCLayer(27, 81, bias=False)
+    flatten = nn.Flatten(0)
+    linear = nn.Linear(27, 81, bias=False)
+    # set same weights
+    fc.set_weights(linear.weight.T.detach().numpy())
+    assert np.alltrue(fc.weights == linear.weight.T.detach().numpy())
+    
+    # one forward pass 
+    fc_output = fc.forward(init_np)
+    flatten_tensor = flatten.forward(init_tensor)
+    linear_output = linear.forward(flatten_tensor)
+    # dtype causes numerical instability, so we round to 5th decimal
+    assert np.alltrue(
+        np.round(fc_output.reshape(81), 5) == np.round(linear_output.detach().numpy(), 5)
+    )
+
+    # one backward pass
+    # initialze output loss
+    grad_np = np.random.randn(1, 81)
+    grad_tensor = torch.tensor(grad_np, dtype=torch.float32)
+
+    input_error = fc.backward(grad_np, learning_rate=.01)
+    input_grad = torch.autograd()
+
+    
