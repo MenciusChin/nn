f0a182b32fd71b26e7678d2075f135579639c48a
MenciusChin <menciuschin@gmail.com>
2024-06-04 10:26:15 -0400

    finish get_im2col_indices

--- im2col.py
+++ im2col.py
@@ -23,25 +23,39 @@ def get_im2col_indices(
     f_H, f_W = filter_shape
 
     # assertion to ensure correct stride and padding
-    assert (in_H + 2 * padding - dilation[0] * (f_H - 1) - 1) % stride == 0
-    assert (in_W + 2 * padding - dilation[1] * (f_W - 1) - 1) % stride == 0
+    assert (in_H + 2 * padding[0] - dilation[0] * (f_H - 1) - 1) % stride[0] == 0
+    assert (in_W + 2 * padding[1] - dilation[1] * (f_W - 1) - 1) % stride[1] == 0
 
     # calculate output shape
     out_H = int((in_H + 2 * padding[0] - dilation[0] * (f_H - 1) - 1) / stride[0] + 1)
     out_W = int((in_W + 2 * padding[1] - dilation[1] * (f_W - 1) - 1) / stride[1] + 1)
 
     # get row and col indices 
-    i0 = np.tile(np.repeat(np.arange(f_H), f_W), C)
-    i1 = stride * np.repeat(np.arange(out_H), out_W)
-    j0 = np.tile(np.arange(f_W), f_H * C)
-    j1 = stride * np.tile(np.arange(out_W), out_H)
+    i0 = np.repeat(np.arange(f_H), f_W)
+    i0 = np.tile(i0, C)
+    i1 = stride[0] * np.repeat(
+        np.arange(out_H * dilation[0], step=dilation[0]), 
+        out_W
+    )
+
+    j0 = np.arange(f_W)
+    j0 = np.tile(j0, f_H * C)
+    j1 = stride[1] * np.tile(
+        np.arange(out_W * dilation[1], step=dilation[1]),
+        out_H
+    )
+
     i = i0.reshape(-1, 1) + i1.reshape(1, -1)
     j = j0.reshape(-1, 1) + j1.reshape(1, -1)
-    print(i, "\n", j)
+    k = np.repeat(np.arange(C), f_H * f_W).reshape(-1, 1)
+
+    return (k, i, j)
+    
+
 
 
 if __name__ == "__main__":
-    get_im2col_indices(
+    print(get_im2col_indices(
         (1, 3, 5, 5),
         (3, 3)
-    )
+    ))
