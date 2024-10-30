ecac6e7386e0ca5e75e77755a4d5adae14d87ab2
Menciuschin <menciuschin@gmail.com>
2024-10-30 12:00:51 -0400

    define MLP

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -5,6 +5,19 @@ from torch.nn import functional as F
 
 # ------------------------------------------------------------------------------
 
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 class Block(nn.Module):
 
@@ -16,10 +29,10 @@ class Block(nn.Module):
         self.mlp = MLP(config)
     
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
+        # pre-normalization
+        x = x + self.attn(self.ln_1(x)) # reduce
+        x = x + self.mlp(self.ln_2(x)) # map
         return x
-
 
 
 @dataclass
