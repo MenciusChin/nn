92016300bb07c6541ffa7b86bb946d0619adddea
Menciuschin <menciuschin@gmail.com>
2024-10-30 11:55:17 -0400

    add Block define;

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -3,7 +3,22 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+# ------------------------------------------------------------------------------
 
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
 
 
 
@@ -29,3 +44,4 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
