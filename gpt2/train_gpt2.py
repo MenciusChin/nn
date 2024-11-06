920cc591a830c0692f7afeeb11b5c1aada533bb2
Menciuschin <menciuschin@gmail.com>
2024-11-06 12:37:35 -0500

    add residual stream std control

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -16,6 +16,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.LLMC_RESIDUAL_SCALE_FLAG = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -51,6 +52,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.LLMC_RESIDUAL_SCALE_FLAG = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -102,10 +104,15 @@ class GPT(nn.Module):
 
         # init params
         self.apply(self._init_weights)
-
+    
     def _init_weights(self, module):
+        # following the gpt-2 source code
         if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            std = 0.02
+            if hasattr(module, 'LLMC_RESIDUAL_SCALE_FLAG'):
+                # control residual stream by multiplying 1/sqrt(n_layer)
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
             elif isinstance(module, nn.Embedding):
