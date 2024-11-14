bfe921994b608fe122c6238fc7d0748bd5418ac2
Menciuschin <menciuschin@gmail.com>
2024-11-14 14:03:28 -0500

    clip norm at 1.0

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -269,12 +269,13 @@ for i in range(50):
     with torch.autocast(device_type=device, dtype=torch.bfloat16):
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     optimizer.step()
     torch.cuda.synchronize() # wait for the GPU to finish work
     t1 = time.time()
     dt = (t1 - t0) * 1000 # time difference in miliseconds
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss: {loss.item()}, dt: {dt: .2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {i} | loss: {loss.item()} | norm: {norm:.4f} | dt: {dt: .2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
